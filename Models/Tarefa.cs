074e1f33d532885c2fdf513c3e363c98c3af6d1e
RHamudi <ramonramos.silva16@gmail.com>
2024-08-21 11:59:35 -0300

    feat: factorie criar tarefa & validacoes

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
+using sQpets_Backend.DTO;
 
 namespace sQpets_Backend.Models
 {
@@ -14,5 +16,25 @@ namespace sQpets_Backend.Models
         public int Tempo { get; set; }
         public Guid IdUsuario { get; set; }
         public Guid IdCategoria { get; set; }
+
+        public static class Factories
+        {
+            public static Tarefa CreateTarefa(CreateTarefaDTO dto)
+            {
+                if(dto.Nome == string.Empty) throw new Exception("Nome não pode estar vazio");
+                if(dto.Tempo < 0) throw new Exception("O campo tempo não pode ser negativo");
+
+                return new Tarefa()
+                {
+                    IdTarefa = Guid.NewGuid(),
+                    Data = DateTime.UtcNow,
+                    Nome = dto.Nome,
+                    Status = false,
+                    Tempo = dto.Tempo,
+                    IdUsuario = dto.IdUsuario,
+                    IdCategoria = dto.IdCategoria
+                };
+            }
+        }
     }
 }
