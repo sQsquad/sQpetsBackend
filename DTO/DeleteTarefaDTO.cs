6538d5ffd2340344e9ff0f6719cd53f6193e6d44
RHamudi <ramonramos.silva16@gmail.com>
2024-08-21 12:00:36 -0300

    feat: dto deletar tarefa

---
+++ DTO/DeleteTarefaDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sQpets_Backend.DTO
+{
+    public class DeleteTarefaDTO
+    {
+        public Guid IdTarefa { get; set; }
+    }
+}
