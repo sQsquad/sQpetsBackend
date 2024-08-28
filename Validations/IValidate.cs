6e40f3aef79ec00be4b80231be4a4ad31c2287a9
RHamudi <ramonramos.silva16@gmail.com>
2024-08-28 10:56:13 -0300

    feat: classe validação

---
+++ Validations/IValidate.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sQpets_Backend.Validations
+{
+    public interface IValidate
+    {
+        public bool IsValid();
+    }
+}
---
+++ Validations/TarefaValdations.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using sQpets_Backend.Models;
+using sQpets_Backend.Notifications;
+
+namespace sQpets_Backend.Validations
+{
+    public class TarefaValdations : IValidate
+    {
+        private readonly Tarefa _tarefa;
+        public TarefaValdations(Tarefa tarefa)
+        {
+            _tarefa = tarefa;
+        }
+
+        public TarefaValdations GuidCategoriaIsValid()
+        {
+            if(!Guid.TryParse(_tarefa.IdCategoria, out Guid c))
+                _tarefa.AddNotification(new Notification("IdCategoria", "Id invalido"));
+            return this;
+        }
+         public TarefaValdations GuidUsuarioIsValid()
+        {
+            if(!Guid.TryParse(_tarefa.IdUsuario, out Guid u))
+                _tarefa.AddNotification(new Notification("IdUsuario", "Id invalido"));
+            return this;
+        }
+        
+        public TarefaValdations NomeValidation()
+        {
+            if(_tarefa.Nome == string.Empty || _tarefa.Nome.Length > 50)
+                _tarefa.AddNotification(new Notification("Nome", "Por favor forneça um nome valido ou menor que 50 caracteres"));
+            return this;
+        }
+
+        public bool IsValid()
+        {
+            return _tarefa.Notifications.Count == 0;
+        }
+    }
+}
